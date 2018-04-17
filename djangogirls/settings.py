2df18e3f98d53ee392042110efba89b811dfdc2d
bibicobolado <rodrigosiqueira@id.uff.br>
2018-04-16 21:09:16 -0300

    Testando após clonar code para pc novo

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g%9#ljnvqybgnv5j=@f)$2$8ij$z6)&g9&i+axo-%%=ktxhnw9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','rodrigosiqueira.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost','rodrigosiqueira.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
