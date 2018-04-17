f64ca43f5ce82572a5af9d9dac591ad0f909cc7b
bibicobolado <rodrigosiqueira@id.uff.br>
2018-04-16 22:59:45 -0300

    change in blog's templates and views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 
+from .models import Post
+
 def home(request):
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
 	template_name='blog/post_list.html'
-	return render (request,template_name)
+	context={}
+	context['posts']=posts
+	return render (request,template_name,context)
